e1701f2286a07e26abbf1d0dfaee8c6ea9005ae9
UltraVi23 <razyuvi@hotmail.com>
2026-05-24 10:55:51 -0700

    Set up project and opencv

---
+++ FinalProject/driver.cpp
@@ -0,0 +1,18 @@
+// driver.cpp
+// by UV Raz and Emily Kim, 5/24/2026 - 6/__/2026
+//
+// Project Description Goes Here
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+#include <iostream>
+
+// main
+// Preconditions: 
+// Postconditions: 
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+}
